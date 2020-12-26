1275b203dc322ab0468b6d493a95357f080ea210
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-26 17:38:17 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,57 @@
-class Person
-end
 
-adele_goldberg = Person.new
-adele_goldberg #=> #
+#  1) Person with names #name= writes the name of the person to an instance variable @name
+   #  Failure/Error: beyonce.name = "Beyonce"
+   
+class Person
+
+def initialize(name, job)
+    
+    @name = name
+    @job = job
+
+  def name=(person_name)
+    @name = person_name
+ 
+  end
+
+  def name
+    @name
+  end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+
+puts beyonce.name
+     
+    # NoMethodError:
+     #  undefined method `name=' for #<Person:0x0000000003011078>
+     
+     
+     
+     
+#  2) Person with names #name reads the name of the person from an instance variable @name
+   #  Failure/Error: expect(beyonce.name).to eq("Beyonce")
+     
+    # NoMethodError:
+     #  undefined method `name' for #<Person:0x000000000301e200 @name="Beyonce">
+     
+     
+     
+     
+     
+# 3) Person with jobs #job= writes the job of the person to an instance variable @job
+   #  Failure/Error: beyonce.job = "Singer"
+     
+    # NoMethodError:
+     #  undefined method `job=' for #<Person:0x0000000003023ef8>
+     
+     
+     
+     
+#   4) Person with jobs #job reads the job of the person from an instance variable @job
+   #  Failure/Error: expect(beyonce.job).to eq("Singer")
+     
+    # NoMethodError:
+     #  undefined method `job' for #<Person:0x0000000003022558 @job="Singer">
+end
+end
